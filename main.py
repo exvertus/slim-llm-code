c290b48816ba75a39a051ac6c9e79720be767058
Thomas Flanigan <tflanig1@gmail.com>
2024-01-20 17:42:15 -0600

    router agent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from langchain.agents import AgentType
+from langchain.agents import AgentType, initialize_agent
+from langchain.tools import Tool
 from langchain_experimental.agents.agent_toolkits import create_python_agent, create_csv_agent
 from langchain_experimental.agents.agent_toolkits.python.prompt import PREFIX
 from langchain_openai import ChatOpenAI
@@ -14,12 +15,11 @@ def main():
     )
 
     # # doesn't seem to be prepending the python code prompt from invoke on its own
-    # # prepending manually fixes
-    # python_agent_executor.invoke(f"""{PREFIX}
+    # # prepending manually fixes it
+    def invoke_py_agent(prompt):
+        python_agent_executor.invoke(f"""{PREFIX}
 
-    #     generate and save in current working directory 15 QRcodes
-    #     that point to www.udemy.com/course/langchain
-    #     you already have the qrcode package installed in the REPL environment""")
+        {prompt}""")
     
     csv_agent = create_csv_agent(
         llm=ChatOpenAI(temperature=0, model="gpt-4"),
@@ -29,7 +29,31 @@ def main():
     )
 
     # csv_agent.invoke("how many columns are there in file episode_info.csv")
-    csv_agent.invoke("print the seasons by ascending number of episodes")
+    # csv_agent.invoke("print the seasons by ascending number of episodes")
+
+    grand_agent = initialize_agent(tools=[
+        Tool(
+            name="PythonAgent",
+            func=invoke_py_agent,
+            description="""useful when you need to transform natural language to a 'python result',
+                        where 'python result' is the end result of writing a python script, running it, and returning the result of its execution
+                        DO NOT SEND PYTHON CODE TO THIS TOOL, its only suitable input is natural language"""
+        ),
+        Tool(
+            name="CSVAgent",
+            func=csv_agent.invoke,
+            description="""useful when you need to answer question about episode_info.csv,
+                        takes an input as a natural language question and returns the answer after running pandas calculations"""
+        )
+    ],
+    llm=ChatOpenAI(temperature=0, model="gpt-4"),
+    agent_type=AgentType.OPENAI_FUNCTIONS,
+    verbose=True
+    )
+
+    grand_agent.invoke("""generate and save in current working directory 15 QRcodes
+                       that point to www.udemy.com/course/langchain, 
+                       you have qrcode package installed already, so there is no need to pip install""")
 
 if __name__ == "__main__":
     main()
