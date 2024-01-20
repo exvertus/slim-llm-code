cda3f601a6ba92ac8bde9d2310991e509bbc7809
Thomas Flanigan <tflanig1@gmail.com>
2024-01-20 13:43:16 -0600

    csv tests working

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from langchain.agents import AgentType
-from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain_experimental.agents.agent_toolkits import create_python_agent, create_csv_agent
 from langchain_experimental.agents.agent_toolkits.python.prompt import PREFIX
 from langchain_openai import ChatOpenAI
 from langchain_experimental.tools import PythonREPLTool
@@ -13,13 +13,23 @@ def main():
         verbose=True
     )
 
-    # doesn't seem to be prepending the python code prompt from invoke on its own
-    # prepending manually fixes
-    python_agent_executor.invoke(f"""{PREFIX}
+    # # doesn't seem to be prepending the python code prompt from invoke on its own
+    # # prepending manually fixes
+    # python_agent_executor.invoke(f"""{PREFIX}
 
-generate and save in current working directory 15 QRcodes
-that point to www.udemy.com/course/langchain
-you already have the qrcode package installed in the REPL environment""")
+    #     generate and save in current working directory 15 QRcodes
+    #     that point to www.udemy.com/course/langchain
+    #     you already have the qrcode package installed in the REPL environment""")
+    
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+    )
+
+    # csv_agent.invoke("how many columns are there in file episode_info.csv")
+    csv_agent.invoke("print the seasons by ascending number of episodes")
 
 if __name__ == "__main__":
     main()
